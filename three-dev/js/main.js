14a513707873bf303517b06588746aac08ef7614
Sohaib Ebrahimi <ebrahimi_sa@icloud.com>
2024-11-19 21:34:41 +0200

    init

--- three-dev/js/main.js
+++ three-dev/js/main.js
@@ -1,72 +1,58 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-let container, camera, scene, renderer, cube, object, objec;
-
-
+let camera, scene, renderer;
 
 init();
-
 function init() {
+  const container = document.createElement( 'div' );
+  document.body.appendChild( container );
+  
+  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
+  camera.position.set( - 1.8, 0.6, 2.7 );
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 1, 2000);
+  new RGBELoader().setPath('./assets/').load('qwantani_sunset_2k.hdr', 
+  function ( texture ) 
+  {texture.mapping = THREE.EquirectangularReflectionMapping; 
+    scene.background = texture; 
+    scene.environment = texture;
 
-  renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  renderer.setAnimationLoop(animate);
-  document.body.appendChild(renderer.domElement);
-
-  const geometry = new THREE.BoxGeometry(1, 1, 1);
-  const material = new THREE.MeshPhongMaterial({color:  0x00ff00,});
-
+	render();
+  // model
+  const loader = new GLTFLoader().setPath( './assets/lightBulbModel/' );
+	loader.load( 'lightbulb_led_2k.gltf', async function ( gltf ) {
+    const model = gltf.scene;
+    // wait until the model can be added to the scene without blocking due to shader compilation
+    await renderer.compileAsync( model, camera, scene );
+    scene.add( model );
+    render();
+  });
+});
+  renderer = new THREE.WebGLRenderer( { antialias: true } );
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1;
+  renderer.toneMapping = THREE.LinearToneMapping;
+  renderer.outputEncoding = THREE.sRGBEncoding;
+  container.appendChild( renderer.domElement );
   const controls = new OrbitControls( camera, renderer.domElement );
-
-  cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
-
-  objec = new THREE.Mesh( new THREE.CylinderGeometry( 2, 2, 1,4,5 ), material );
-	objec.position.set( -2, 1, 1 );
-
-  object = new THREE.Mesh( new THREE.SphereGeometry( 2, 1, 1 ), material );
-	object.position.set( 2, -1, 1 );
-	scene.add( object );
-
-  camera.position.set(5, 5, 5);
+  controls.addEventListener( 'change', render ); // use if there is no animation loop
+  controls.minDistance = 2;
+  controls.maxDistance = 10;
+  controls.target.set( 0, 0, - 0.2 );
   controls.update();
-
-  const axesHelper = new THREE.AxesHelper(5);
-  scene.add(axesHelper);
-
-  camera.lookAt(axesHelper.position);
-
-  const light = new THREE.DirectionalLight( 'white' );
-  scene.add( light );
-
-  const ambientLight = new THREE.AmbientLight( 0xcccccc, 1.5 );
-	scene.add( ambientLight );
-  const pointLight = new THREE.PointLight( 0xffffff, 2.5, 0, 0 );
-	camera.add( pointLight );
-  const helper = new THREE.DirectionalLightHelper( light, 20 );
-  scene.add( helper );
-  scene.add( camera );
-  scene.add(objec);
+  window.addEventListener( 'resize', onWindowResize );
 }
-
-function animate() {
-  cube.rotation.x += 0.1;
-  cube.rotation.y += 0.001;
-  object.rotation.x += 0.1;
-  object.rotation.y += 0.1;
-  objec.rotation.x += 0.1;
-  objec.rotation.y += 0.1;
-
-  renderer.render(scene, camera);
-}
-
-window.addEventListener('resize', resize, false);
-
-function resize() {
+function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
+	camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  render();
+}
+//
+function render() {
+  renderer.render( scene, camera );
 }
