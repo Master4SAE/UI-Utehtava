6a43c8da9f743eb50e6f275a0b4990a709c4dc8b
Sohaib Ebrahimi <ebrahimi_sa@icloud.com>
2024-11-20 18:52:49 +0200

    changes

--- three-dev/js/main.js
+++ three-dev/js/main.js
@@ -3,56 +3,85 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
-let camera, scene, renderer;
+let camera, scene, renderer, controls;
+let lightBulbModel;
+let vk2SceneModel;
 
 init();
-function init() {
-  const container = document.createElement( 'div' );
-  document.body.appendChild( container );
-  
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.25, 20 );
-  camera.position.set( - 1.8, 0.6, 2.7 );
-  scene = new THREE.Scene();
-  new RGBELoader().setPath('./assets/').load('qwantani_sunset_2k.hdr', 
-  function ( texture ) 
-  {texture.mapping = THREE.EquirectangularReflectionMapping; 
-    scene.background = texture; 
-    scene.environment = texture;
 
-	render();
-  // model
-  const loader = new GLTFLoader().setPath( './assets/lightBulbModel/' );
-	loader.load( 'lightbulb_led_2k.gltf', async function ( gltf ) {
-    const model = gltf.scene;
-    // wait until the model can be added to the scene without blocking due to shader compilation
-    await renderer.compileAsync( model, camera, scene );
-    scene.add( model );
+function init() {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+  const loadingManager = new THREE.LoadingManager();
+  loadingManager.onLoad = function () {
     render();
+    animate();
+  };
+  camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 2000);
+  scene = new THREE.Scene();
+  const rgbeLoader = new RGBELoader(loadingManager);
+  rgbeLoader.setPath('./assets/').load('qwantani_sunset_2k.hdr', function (texture) {
+    texture.mapping = THREE.EquirectangularReflectionMapping;
+    scene.background = texture;
+    scene.environment = texture;
+    const loader = new GLTFLoader(loadingManager).setPath('./assets/');
+    loader.load('VK2Scene.glb', function (gltf) {
+      vk2SceneModel = gltf.scene;
+      const box = new THREE.Box3().setFromObject(vk2SceneModel);
+      const boxCenter = box.getCenter(new THREE.Vector3());
+      const boxSize = box.getSize(new THREE.Vector3());
+      vk2SceneModel.position.sub(boxCenter);
+      scene.add(vk2SceneModel);
+      controls.target.copy(boxCenter);
+      camera.position.set(
+        boxCenter.x + boxSize.x,
+        boxCenter.y + boxSize.y,
+        boxCenter.z + boxSize.z
+      );
+      controls.update();
+    });
+    loader.load('/lightBulbModel/lightbulb_led_2k.gltf', function (gltf) {
+      lightBulbModel = gltf.scene;
+      const box = new THREE.Box3().setFromObject(lightBulbModel);
+      const boxCenter = box.getCenter(new THREE.Vector3());
+      const boxSize = box.getSize(new THREE.Vector3());
+      lightBulbModel.position.sub(boxCenter);
+      lightBulbModel.position.x += 25;
+      lightBulbModel.position.y += 5; 
+      lightBulbModel.scale.set(150, 150, 150);
+      scene.add(lightBulbModel);
+      controls.target.copy(boxCenter);
+      camera.position.set(
+        boxCenter.x + boxSize.x,
+        boxCenter.y + boxSize.y,
+        boxCenter.z + boxSize.z
+      );
+      controls.update();
+    });
   });
-});
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.toneMapping = THREE.ACESFilmicToneMapping;
-  renderer.toneMappingExposure = 1;
-  renderer.toneMapping = THREE.LinearToneMapping;
   renderer.outputEncoding = THREE.sRGBEncoding;
-  container.appendChild( renderer.domElement );
-  const controls = new OrbitControls( camera, renderer.domElement );
-  controls.addEventListener( 'change', render ); // use if there is no animation loop
-  controls.minDistance = 2;
-  controls.maxDistance = 10;
-  controls.target.set( 0, 0, - 0.2 );
-  controls.update();
-  window.addEventListener( 'resize', onWindowResize );
+  container.appendChild(renderer.domElement);
+  controls = new OrbitControls(camera, renderer.domElement);
+  controls.addEventListener('change', render);
+  window.addEventListener('resize', onWindowResize);
 }
 function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
-	camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
   render();
 }
-//
+function animate() {
+  requestAnimationFrame(animate);
+  if (lightBulbModel) {
+    lightBulbModel.rotation.y += 0.01;
+  }
+  render();
+}
 function render() {
-  renderer.render( scene, camera );
+  renderer.render(scene, camera);
 }
